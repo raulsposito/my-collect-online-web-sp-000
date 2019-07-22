7da2b72ec3a7599067ab84ed04dc013ae770953e
Raul Sposito <raulsposito@gmail.com>
2019-07-22 14:52:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,17 +1,15 @@
-def my_collect(array) 
-  my_collect(array) do |name|
-  name.split(" ").first 
-end
-  i = 0
-  while i < array.length
-  collection << 
-  yield(array[i])
-    i + 1 
-  end
-  collection
-end
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+my_collect(list) {|i| i.split(" ").first}
 
 
 
