a714deef3c623eb81d14a21692c5a54648740ff7
Raul Sposito <raulsposito@gmail.com>
2019-07-22 14:51:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array) 
+  my_collect(array) do |name|
+  name.split(" ").first 
+end
+  i = 0
+  while i < array.length
+  collection << 
+  yield(array[i])
+    i + 1 
+  end
+  collection
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
 
 
